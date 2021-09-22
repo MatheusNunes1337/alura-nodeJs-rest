2fb07ee10b14ed3546f6aa56f662f7eb5b1db24c
Matheus <matheusyagami147@gmail.com>
2021-09-22 15:37:41 -0300

    criada a rota post de atendimentos e configurado o bodyParser no arquivo de config do express

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,4 +2,9 @@ module.exports = app => {
     app.get('/atendimentos', (req, res) => {
         res.send('você está na rota de atendimentos')
     })
+
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('você está na rota de post de atendimentos')
+    })
 }
