e73e5b77202b10624682be0f778fe0aaa1ce65a2
Matheus <matheusyagami147@gmail.com>
2021-09-23 13:24:10 -0300

    criado o diretório de models e o arquivo de atendimentos dentro dele. Método adiciona criado na classe de Atendimento (model)

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,10 +1,13 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
     app.get('/atendimentos', (req, res) => {
         res.send('você está na rota de atendimentos')
     })
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body)
-        res.send('você está na rota de post de atendimentos')
+        const atendimento = req.body
+        Atendimento.adiciona(atendimento)
+        res.send('Post atendimento')
     })
 }
